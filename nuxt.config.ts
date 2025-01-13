a476b7c64762a494386ec4bdb727606319b34bfc
Mira <r1n0ku@yandex.ru>
2025-01-13 20:55:08 +0100

    feat: making ready to deploy to cloudflare pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,19 +17,19 @@ export default defineNuxtConfig({
     layoutTransition: { name: 'transition-page', mode: 'out-in' }
   },
 
-  // $production: {
-  //   nitro: {
-  //     preset: 'cloudflare-pages-static'
-  //   },
-  //   routeRules: {
-  //     '/': { prerender: true }
-  //   }
-  // },
+  $production: {
+    nitro: {
+      preset: 'cloudflare-pages-static'
+    },
+    routeRules: {
+      '/': { prerender: true }
+    }
+  },
 
   runtimeConfig: {
     public: {
-      apiUrl: 'http://localhost:7000',
-      discordOAuthUrl: '', // TODO: Remove before push
+      apiUrl: 'https://rfad-se-backend.rinisle.uk',
+      discordOAuthUrl: 'https://discord.com/oauth2/authorize?client_id=1320082838951034910&response_type=code&redirect_uri=https%3A%2F%2Frfad-mods.rinisle.uk%2Foauth2&scope=identify',
       cdnUrl: 'https://rfad-images-test.rinisle.uk/images/', // https://images.example.com/images/ОСТАВИТЬ_ПУСТЫМ, сюда будет писаться хеш
       boostyUrl: 'https://boosty.to/skyrim_rfad_chicken'
     }
